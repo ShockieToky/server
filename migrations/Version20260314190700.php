4e2c9f95942e9f290d2d1710f10e568a0256ea6d
ShockieToky <aymerick.guerinet@laposte.net>
2026-03-20 16:16:06 +0100

    feat: update production environment variables for API and CORS settings

--- migrations/Version20260314190700.php
+++ migrations/Version20260314190700.php
@@ -20,9 +20,8 @@ final class Version20260314190700 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE messenger_messages');
-        $this->addSql('ALTER TABLE user ADD pseudo VARCHAR(50) NOT NULL, ADD role VARCHAR(20) NOT NULL, ADD is_active TINYINT DEFAULT 1 NOT NULL, DROP roles');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D64986CC499D ON user (pseudo)');
+        $this->addSql("DROP TABLE IF EXISTS messenger_messages");
+        $this->addSql('CREATE TABLE IF NOT EXISTS user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(255) NOT NULL, pseudo VARCHAR(50) NOT NULL, role VARCHAR(20) NOT NULL, is_active TINYINT DEFAULT 1 NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D64986CC499D (pseudo), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
